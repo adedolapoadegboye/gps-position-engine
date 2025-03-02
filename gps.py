c3556f6f450c3c7bcef63a4b4a52bbda883e3732
Adedolapo Adegboye <adedolapoadegboye@Adedolapos-Mac-mini.local>
2025-03-01 16:44:14 -0800

    v0.1

--- gps.py
+++ gps.py
@@ -38,13 +38,13 @@ def main():
             print(eph_data) if DEBUG else None
 
             formatted_eph_data = format_ephemeris(eph_data)
-            print(f"EPH: {formatted_eph_data}")
+            print(f"EPH: {formatted_eph_data}") if DEBUG else None
 
             formatted_pr_data = format_pseudorange(pr_data)
-            print(f"PR: {formatted_pr_data}")
+            print(f"PR: {formatted_pr_data}") if DEBUG else None
 
             receiver_tow = format_receiver_tow(pr_data)
-            print(f"Receiver TOW: {receiver_tow}")
+            print(f"Receiver TOW: {receiver_tow}") if DEBUG else None
 
             if formatted_pr_data.keys() == formatted_eph_data.keys() and len(formatted_eph_data) > 4:
                 # If all satellites with pseudorange values have corresponding EPH data and more than 4 satellites are visible
@@ -59,7 +59,7 @@ def main():
                     corrected_pseudorange = pseudorange + (SPEED_OF_LIGHT * sat_clk_bias)
                     formatted_corrected_pseudorange_data[sv_id] = corrected_pseudorange
 
-                print(f"Sat CLK Corrected PR: {formatted_corrected_pseudorange_data}")
+                print(f"Sat CLK Corrected PR: {formatted_corrected_pseudorange_data}") if DEBUG else None
 
                 dx = np.array([100, 100, 100])  # 1D array
                 ds_ = np.array([100, 100, 100])  # 1D array
@@ -80,7 +80,7 @@ def main():
 
                         # Get satellite position
                         ds_ = get_satellite_position(formatted_eph_data[svid], satellite_tow)
-                        print(f"Satellite {svid} ECEF Position: {ds_}")
+                        print(f"Satellite {svid} ECEF Position: {ds_}") if DEBUG else None
                         theta = OMEGA_E * tau
                         print(f"Satellite {svid} theta: {theta}") if DEBUG else None
 
@@ -88,8 +88,8 @@ def main():
                         Xs = np.vstack((Xs, ds_))
 
                     print(f"Xs Shape: {Xs.shape}, pr Shape: {pr.shape}") if DEBUG else None
-                    print(f"Xs: {Xs}")
-                    print(f"pr: {pr}")
+                    print(f"Xs: {Xs}") if DEBUG else None
+                    print(f"pr: {pr}") if DEBUG else None
 
                     xu = np.array(xu, dtype=np.float64)
                     b = float(b)
@@ -100,9 +100,9 @@ def main():
                     db = b_ - b
                     xu = x_
                     b = b_
-                    print(f"xu: {xu}")
-                    print(f"norm_dp: {norm_dp}")
-                    print(f"G: {G}")
+                    print(f"xu: {xu}") if DEBUG else None
+                    print(f"norm_dp: {norm_dp}") if DEBUG else None
+                    print(f"G: {G}") if DEBUG else None
 
 
                 lambda_, phi, h = WGStoEllipsoid(xu[0], xu[1], xu[2])
